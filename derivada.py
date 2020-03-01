987a5c620c8c3f100853104fca11eed0bad19a02
Gabriela Cavalcante da Silva <gabicavalcantesilva@gmail.com>
2020-03-01 00:18:41 -0300

    add readme

--- derivada.py
+++ derivada.py
@@ -7,4 +7,3 @@ lim = lambda x, h: (f(x + h) - f(x))/h
 
 for h in [0.1, 0.05, 0.01, 0.005, 0.0001, 0.00005]:
     print(f"h: {h} -> {lim(1, h)}")
-f''
--- l1e1.py
+++ l1e1.py
@@ -1,12 +1,16 @@
 import PyGnuplot as gp
 import numpy as np
+from math import *
 
 def f(x):
     return (x**3) - (2* (x**2)) - x + 2
 
-f_ = lambda x, h: (f(x + h) - f(x))/h
+def g(x):
+    return sin(x) * cos(x) + exp(2*x) + 2*x**4 - 10
 
-tang = [f_(1, i) for i in [0.1, 0.05, 0.01, 0.005, 0.001, 0.0005, 0.0001]]
+derive = lambda function, x, h: (function(x + h) - function(x))/h
+
+tang = [derive(f, 1, i) for i in [10**x for x in range(0, -12, -1)]]
 
 gp.s([tang], filename="tangente.dat")
 
@@ -15,7 +19,5 @@ gp.c('set xrange[-1.5:2.5]')
 gp.c('set xzeroaxis')
 gp.c('set yzeroaxis')
 gp.c('plot (x**3) - (2* (x**2)) - x + 2 title "função cúbica"')
-
-c_plot = 'replot "tangente.dat" with lines '
-gp.c(c_plot)
+gp.c('replot "tangente.dat" with lines ')
 
