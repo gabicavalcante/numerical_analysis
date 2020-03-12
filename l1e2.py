3e6e1988808cb9e16bd905926a8562650f65b04c
Gabriela Cavalcante da Silva <gabicavalcantesilva@gmail.com>
2020-03-11 22:53:54 -0300

    finish e2

--- l1e2.py
+++ l1e2.py
@@ -6,19 +6,14 @@ derive = lambda x: cos(x) - x * sin(x)
 # f_xh = lambda f_x, h, x: f_x + h * derive(x)
  
 def get_point(x, h, fx): 
-    xh = x + h
-        
+    xh = x + h        
     f_xh = fx + h * derive(x)  
     x = xh
 
-    return (x, f_xh)
-
-    #points_x.append(x)
-    #points_y.append(f_xh) 
-
-    fx = f_xh
+    return (x, f_xh) 
+   
     
-def negative_points():
+def points():
     x = 0 
     h = -0.5
     fx = 1
@@ -27,18 +22,24 @@ def negative_points():
     points_y = []
     
     while x > -6:
-        xh = x + h
-        
-        f_xh = fx + h * derive(x)  
-        x = xh
-
+        x, f_xh = get_point(x, h, fx)  
         points_x.append(x)
         points_y.append(f_xh) 
 
         fx = f_xh
 
-    return [points_x, points_y]
+    x = 0 
+    h = 0.5
+    fx = 1
+    
+    while x < 6:
+        x, f_xh = get_point(x, h, fx)  
+        points_x.append(x)
+        points_y.append(f_xh) 
 
+        fx = f_xh
+
+    return(points_x, points_y)
 
 
 
@@ -52,5 +53,11 @@ def plot_graph(a, b):
 
     gp.c(f'plot "<echo 0 1" title "(0, 1)"')
 
+    gp.s(points(), filename="arquivos.pts")
 
-negative_points()
+    gp.c('replot "arquivos.pts" with lines title "f(x) no intervalo [−6:6]"')
+    gp.c('replot x*cos(x) + 1 title "f(x) = x*cos(x) + 1"')
+
+
+if __name__ == '__main__': 
+    plot_graph(-6, 6)
